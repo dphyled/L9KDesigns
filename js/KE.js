918a87c9f65a7579bfcc4036ce0dc2f1859d5b7b
George K. Custer <custer@tuj.ac.jp>
2016-11-19 17:01:14 +0900

    Add birdsgit add .

--- js/KE.js
+++ js/KE.js
@@ -12,14 +12,12 @@ $(document).ready(function() {
         onLeave: function(index, nextIndex, direction){
             var leavingSection = $(this);
             if(nextIndex == 1){
-		    	TweenMax.to($(".main-menu-item"), .5, {height: "4em"});
-		    	TweenMax.to($(".main-menu-item"), .5, {lineHeight: "4em"});
+		    	TweenMax.to($(".main-menu-item"), .5, {height: "4em", lineHeight: "4em", fontSize: "2em"});
 		    	TweenMax.to($(".logo"), .5, {height: "6.5em"});
 		    	TweenMax.to($(".header-filler"), .8, {opacity: 0});                
             }
             else{
-		    	TweenMax.to($(".main-menu-item"), .5, {height: "3.25em"});
-		    	TweenMax.to($(".main-menu-item"), .5, {lineHeight: "3.25em"});
+		    	TweenMax.to($(".main-menu-item"), .5, {height: "3.25em", lineHeight: "3.25em", fontSize: "1.6em"});
 		    	TweenMax.to($(".logo"), .5, {height: "5.5em"});
 		    	TweenMax.to($(".header-filler"), .8, {opacity: 1});	    
             }
@@ -85,6 +83,7 @@ $(document).ready(function() {
 
 
 	// ----------Animations with TweenMax and Scroll Magic------------
+	// ----------Initial Page load fade in----------
 	TweenMax.to($("#fullpage"), .05, {opacity: 1, delay: .1});
 	TweenMax.from(".logo", .5, {opacity: 0, scale: 0, ease:Bounce.easeOut, delay: .5});
 	TweenMax.from($(".slide1 div:not(#slide1-1), .header"), 2.5, {opacity: 0, delay: .3});
@@ -92,8 +91,79 @@ $(document).ready(function() {
 	TweenMax.to($(".logo"), .5, {transition: ".5s all", delay: 1.25});
 
 
+
+
+	var bird1XScalar = 1;
+	var bird1YScalar = 1;
+
+	var bird1Flightpath = {
+		soar : {
+			curviness: 1.25,
+			autoRotate: true,
+			values: [
+					{x: bird1XScalar * 0,	y: bird1YScalar * 0},
+					{x: bird1XScalar * 40,	y: bird1YScalar * 0},
+					{x: bird1XScalar * 90,	y: bird1YScalar * 10},
+					{x: bird1XScalar * 150,	y: bird1YScalar * 30},
+					{x: bird1XScalar * 250,	y: bird1YScalar * 60},
+					{x: bird1XScalar * 350,	y: bird1YScalar * 75},
+					{x: bird1XScalar * 450,	y: bird1YScalar * 80}
+				]
+		},
+		looping : {
+			curviness: 1.25,
+			autoRotate: true,
+			values: [
+					{x: bird1XScalar * 550,	y: bird1YScalar * 80},
+					{x: bird1XScalar * 650,	y: bird1YScalar * 0},
+					{x: bird1XScalar * 550,	y: bird1YScalar * -80},
+					{x: bird1XScalar * 450,	y: bird1YScalar * 0},
+					{x: bird1XScalar * 550,	y: bird1YScalar * 30},
+					{x: bird1XScalar * 625,	y: bird1YScalar * 25}
+				]
+		},
+		return : {
+			curviness: 1.25,
+			autoRotate: true,
+			values: [
+					{x: bird1XScalar * 665,	y: bird1YScalar * 20},
+					{x: bird1XScalar * 680,	y: bird1YScalar * 25},
+					{x: bird1XScalar * 690,	y: bird1YScalar * 20},
+					{x: bird1XScalar * 670,	y: bird1YScalar * 18},
+					{x: bird1XScalar * 630,	y: bird1YScalar * 16},
+					{x: bird1XScalar * 500,	y: bird1YScalar * 15},
+					{x: bird1XScalar * 350,	y: bird1YScalar * 10},
+					{x: bird1XScalar * 200,	y: bird1YScalar * 6},
+					{x: bird1XScalar * 100,	y: bird1YScalar * 0},
+					{x: bird1XScalar * -50,	y: bird1YScalar * 5},
+					{x: bird1XScalar * -25,	y: bird1YScalar * 5},
+					{x: bird1XScalar * -0,	y: bird1YScalar * 0}
+
+				]
+		}
+	};
+
+
+
+	var bird1Flight = new TimelineMax({repeat:-1})
+		.add(TweenMax.to($("#bird1"), 9, {css:{bezier:bird1Flightpath.soar, scale: .9}, ease:Power1.easeIn}))
+		.add(TweenMax.to($("#bird1"), 5, {css:{bezier:bird1Flightpath.looping, scale: .7}}))
+		.add(TweenMax.to($("#bird1"), 20, {css:{bezier:bird1Flightpath.return, scale: 1}}));
+	bird1Flight.play(2);
+
+	var bird2Flight = new TimelineMax({repeat:-1})
+		.add(TweenMax.to($("#bird2"), 12, {css:{bezier:bird1Flightpath.soar, scale: 1.1}, ease:Power1.easeIn}))
+		.add(TweenMax.to($("#bird2"), 9, {css:{bezier:bird1Flightpath.looping, scale: 1.2}}))
+		.add(TweenMax.to($("#bird2"), 28, {css:{bezier:bird1Flightpath.return, scale: 1}}));
+	//bird2Flight.play(28);
+	bird2Flight.play(8);
+
+	// ----------Initialize ScrollMagic Controller----------
 	var controller = new ScrollMagic.Controller();
 
+
+
+	// ----------Profile Page animations----------
 	var profileTL = new TimelineMax();
 	profileTL.from("#kelly-image", .75, {transform: "translateX(-2000px)", scale: 0, ease:Bounce.easeOut})
 			 .from("#eriya-image", .75, {transform: "translateX(2000px)", scale: 0, ease:Bounce.easeOut}, 0)
@@ -103,6 +173,9 @@ $(document).ready(function() {
 	var profileScene = new ScrollMagic.Scene({triggerElement: "#profile-trigger", duration: 0})
 								.setTween(profileTL)
 								.addTo(controller);
+
+
+
 
 
 
