5597a049a5fa8d869dfd5757866d653d483171f1
Kelly Custer <dphyled@hotmail.com>
2017-02-17 21:06:58 +0900

    Organize and document JS a bit

--- components/scripts/KE.js
+++ components/scripts/KE.js
@@ -2,15 +2,27 @@ var jQuery = require('jquery');
 
 // Bind $ to jQuery
 (function($){
-	// Once all images and resources have loaded...
+
+	// ---------- Once DOM has loaded... ----------
+	$(document).ready(function() {
+		initializeFullPageJS();
+		initializeParallaxJS();
+		scrollMagicController = initializeScrollMagicJS();
+		setupMobileMenu();
+		setupBirds();
+		setupProfilePage(scrollMagicController);
+		setupContactForm();
+		setupOtherPageElements();
+	}); //End of $(document).ready
+	
+	// ---------- Once all images and resources have loaded... ----------
 	window.onload = function() {
-		// Hide the loading screen overlay
+		// ---------- Hide the loading screen overlay ----------
 		loading = $('#loading-container');
 		TweenMax.to(loading, .3, {opacity: 0});
 		$(loading).css("pointer-events", "none");
 
-		// ----------Animations with TweenMax and Scroll Magic------------
-		// ----------Initial Page load fade in----------
+		// ---------- Initial Page load fade in ----------
 		TweenMax.to($("#fullpage"), .3, {opacity: 1, delay: .1});
 		TweenMax.from(".logo", .5, {opacity: 0, scale: 0, ease:Bounce.easeOut, delay: .5});
 		TweenMax.from($(".slide1 div:not(#slide1-1), .header"), 2.5, {opacity: 0, delay: .3});
@@ -18,9 +30,10 @@ var jQuery = require('jquery');
 		TweenMax.to($(".logo"), .5, {transition: ".5s all", delay: 1.25});
 	}
 
+	
 
-	$(document).ready(function() {
-		// ----------Initilization for fullPage.js------------
+	// ---------- Initialize fullPage.js ------------
+	function initializeFullPageJS() {
 		$('#fullpage').fullpage({
 			scrollBar: true,
 			anchors: ['home', 'about', 'work', 'feed', 'contact'],
@@ -37,37 +50,31 @@ var jQuery = require('jquery');
 			afterLoad: function(anchorLink, index){
 				// Set header/logo for first slide transitions
 				if(index == 1){
-					TweenMax.to($(".main-menu-item"), .5, {height: "4em", lineHeight: "4em", fontSize: "2em"});
-					TweenMax.to($("#main-menu-logo"), .5, {height: "6em"});
-					TweenMax.to($(".header-filler"), .8, {opacity: 0});
+					expandMainMenu();
 				}      
 			},
 
 			onLeave: function(index, nextIndex, direction){
 				// Set header/logo sizes depending on whether next slide is top slide or not
 				if(nextIndex == 1){
-					TweenMax.to($(".main-menu-item"), .5, {height: "4em", lineHeight: "4em", fontSize: "2em"});
-					TweenMax.to($("#main-menu-logo"), .5, {height: "6em"});
-					TweenMax.to($(".header-filler"), .8, {opacity: 0});
+					expandMainMenu();
 				}
 				else{
-					TweenMax.to($(".main-menu-item"), .5, {height: "3.25em", lineHeight: "3.25em", fontSize: "1.6em"});
-					TweenMax.to($("#main-menu-logo"), .5, {height: "5.5em"});
-					TweenMax.to($(".header-filler"), .8, {opacity: 1});	    
+					shrinkMainMenu();
 				}
 
-		//         // Reset the slow zoom effect when scrolling to slide 2
-		//         if(nextIndex == 2) {
-					// slowZoomScene = new ScrollMagic.Scene({triggerElement: "#profile-trigger", duration: 0})
-					// 							.setTween(slowZoomIn)
-					// 							.addTo(controller);
-		//         }
+				// Reset the slow zoom effect when scrolling to slide 2
+				// if(nextIndex == 2) {
+				// 	slowZoomScene = new ScrollMagic.Scene({triggerElement: "#profile-trigger", duration: 0})
+				// 	.setTween(slowZoomIn)
+				// 	.addTo(controller);
+				// }
 			}
 		});
 
-
-		// ----------If any of the sections are taller than the screen, make sure the site scrolls normally------------
-		// ----------(Disable auto scrolling and fitting window to section with fullPage.js)------------
+		// If any of the sections are taller than the window, 
+		// then make sure the site scrolls normally
+		// (Disable auto scrolling and fitting window to section with fullPage.js)
 		var sections = $(".section");
 		$.each(sections, function(i, section) {
 			if($(section).height() > $(window).height()) {
@@ -75,12 +82,10 @@ var jQuery = require('jquery');
 				$.fn.fullpage.setAutoScrolling(false);
 			}
 		});
+	}
 
-
-		
-		
-		
-		// ----------Initilization for parallax.js------------
+	// ---------- Initilization for parallax.js ------------
+	function initializeParallaxJS() {
 		var scene = document.getElementById('main-scene');
 		var parallax = new Parallax(scene, {
 			scalarX: 30,
@@ -88,20 +93,48 @@ var jQuery = require('jquery');
 			frictionX: 0.02,
 			frictionY: 0.02,
 		});
+	}
 
+	// ---------- Initialize ScrollMagic Controller ----------
+	function initializeScrollMagicJS() {
+		var controller = new ScrollMagic.Controller();
+		return controller;
+	}
 
-		
-		
-		
-		// --------------------Birds Flying--------------------
+	// ---------- Setup Mobile Menu and Mobile Menu Trigger ----------
+	function setupMobileMenu() {
+		// On Mobile trigger, add "active" class to menu and trigger.
+		// Prevent page from scrolling
+		$("#mobile-menu-trigger").click(function() {
+			$("#mobile-menu").toggleClass("active");
+			$("#mobile-menu-trigger").toggleClass("active");
+			if($(this).hasClass("active")) {
+				$("body").addClass("noScroll");
+			} else {
+				$("body").removeClass("noScroll");
+			}
+		});
+
+		// When clicking a mobile menu item, or the close button, remove classes
+		$("#close-mobile-menu, .mobile-menu-item").click(function() {
+			$("#mobile-menu").toggleClass("active");
+			$("#mobile-menu-trigger").toggleClass("active");
+			$("body").removeClass("noScroll");
+		});
+	}
+
+	// ---------- Setup flying birds on top slide ----------
+	function setupBirds() {
+		// Get birds
 		var bird1 = $("#bird1");
 		var bird2 = $("#bird2");
 		var bird3 = $("#bird3");
 		
-		//set these to window width/height divided by 100 to make flight path values into percentages
+		// Set these to window width/height divided by 100 to make flight path values into percentages of window
 		var birdXScalar = $(window).width() / 100;
 		var birdYScalar = $(window).height() / 100;
 
+		// Define curves for flight path
 		var birdFlightpath = {
 			soar : {
 				curviness: 1.25,
@@ -143,11 +176,17 @@ var jQuery = require('jquery');
 			}
 		};
 
+		// Flight for first bird, repeat forever
 		var bird1Flight = new TimelineMax({repeat:-1})
+			// Soar to the right for 25 seconds, also shrink to add depth
 			.add(TweenMax.to(bird1, 25, {css:{bezier:birdFlightpath.soar, scale: .6}, ease:Power1.easeInOut}))
+			// flip around to fly back
 			.add(TweenMax.to(bird1, .01, {rotationX: 180}))
+			// Return over the course of 30 seconds and return to original height for repeat
 			.add(TweenMax.to(bird1, 30, {css:{bezier:birdFlightpath.return, scale: 1}, ease:Power1.easeInOut}))
+			// flip back around
 			.add(TweenMax.to(bird1, .01, {rotationX: 0}));
+		// start 3 seconds into animation (birds are staggered at different points in their animations)			
 		bird1Flight.play(3);
 
 		var bird2Flight = new TimelineMax({repeat:-1})
@@ -162,67 +201,33 @@ var jQuery = require('jquery');
 			.add(TweenMax.to(bird3, .01, {rotationX: 180}))
 			.add(TweenMax.to(bird3, 29, {css:{bezier:birdFlightpath.return, scale: 1}, ease:Power1.easeInOut}))
 			.add(TweenMax.to(bird3, .01, {rotationX: 0}));
-		bird3Flight.play(37);
+		bird3Flight.play(37);		
+	}	
 
-
-
-
-
-		// ----------Initialize ScrollMagic Controller----------
-		var controller = new ScrollMagic.Controller();
-
-
+	// ---------- Setup profile page events ----------
+	function setupProfilePage(scrollMagicController) {
 		// ---------- About/Profile Page animations----------
 		// ---------- Pictures and names fly in----------
 		var profileTL = new TimelineMax();
 		profileTL.from("#kelly-image", .55, {scale: 0, ease:Bounce.easeOut})
 				.from("#eriya-image", .55, {scale: 0, ease:Bounce.easeOut}, 0)
-				.from(".name-label.kelly", .2, {transform: "translateX(-2000px)"}, .40)
-				.from(".name-label.eriya", .2, {transform: "translateX(2000px)"}, .40); //target, duration, props, startTime
-
-		// // ---------- Zoom Effect----------
-		// var slowZoomIn = new TimelineMax()
-		// 	.add(TweenMax.to($("#slide2 .in-slide-container"), 15, {scale: 1.1}));
+				.from(".name-label.kelly", .2, {transform: "translateX(-2000px)", opacity: 0}, .40)
+				.from(".name-label.eriya", .2, {transform: "translateX(2000px)", opacity: 0}, .40); //target, duration, props, startTime
 
 		var profileScene = new ScrollMagic.Scene({triggerElement: "#profile-trigger", duration: 0})
 									.setTween(profileTL)
-									.addTo(controller);
+									.addTo(scrollMagicController);
+
+		// // ---------- Zoom Effect ----------
+		// var slowZoomIn = new TimelineMax()
+		// 	.add(TweenMax.to($("#slide2 .in-slide-container"), 15, {scale: 1.1}));
 
 		// // Slow zoom scene also added in onLeave event of FullPage init, so that it resets when entering slide 3
 		// var slowZoomScene = new ScrollMagic.Scene({triggerElement: "#profile-trigger", duration: 0})
 		// 							.setTween(slowZoomIn)
 		// 							.addTo(controller);
 
-
-
-
-
-		// Setup other page elements
-		$(".more-arrow").click(function() {
-			$.fn.fullpage.moveSectionDown();
-		});
-
-
-		$("#mobile-menu-trigger").click(function() {
-			$("#mobile-menu").toggleClass("active");
-			$("#mobile-menu-trigger").toggleClass("active");
-			if($(this).hasClass("active")) {
-				$("body").addClass("noScroll");
-			} else {
-				$("body").removeClass("noScroll");
-			}
-
-		});
-
-		$("#close-mobile-menu, .mobile-menu-item").click(function() {
-			$("#mobile-menu").toggleClass("active");
-			$("#mobile-menu-trigger").toggleClass("active");
-			$("body").removeClass("noScroll");
-		});
-
-
-
-
+		// When clicking on profile image or name label, display the bio and prevent page scrolling
 		$("#kelly-image img, #kelly-image .name-label").click(function() {
 			$("#profile-bio-kelly").addClass("active");
 			$("body").addClass("noScroll");
@@ -233,31 +238,33 @@ var jQuery = require('jquery');
 			$("body").addClass("noScroll");
 		});
 
+		// Close bio when the X is clicked
 		$(".profile-bio .close-x").click(function() {
 			$(".profile-bio").removeClass("active");
 			$("body").removeClass("noScroll");
 		});
+	}
 
 
-
-
-		// Code for contact form
+	// ---------- Setup submit action for contact form ----------
+	function setupContactForm() {
 		var form = $('#contact-form');
 		var submitButton = $('#contact-submit');
 
 		$(form).submit(function(event) {
-			submitButton.html('Sending...');
-
 			event.preventDefault();
+			submitButton.html('Sending...');
 
 			// Serialize form data
 			var formData = $(form).serialize();
 
+			// Send data to contact.php for processing
 			$.ajax({
 				type: 'POST',
 				url: $(form).attr('action'),
 				data: formData
 			})
+			// if successful, clear fields and hide form, then display response
 			.done(function(response) {
 				$('#name').val('');
 				$('#email').val('');
@@ -266,6 +273,7 @@ var jQuery = require('jquery');
 				$(form).slideUp("fast");
 				$('#contact-form-response').html(response).hide().slideDown("fast");
 			})
+			// if it fails, then display error
 			.fail(function(data) {
 				submitButton.html('Error... :(');
 
@@ -275,7 +283,30 @@ var jQuery = require('jquery');
 					$('#contact-form-response').html('Woops.. An error has occured.');
 				}
 			});
-
 		});
-	});
-})(jQuery); // End of making jQuery safe?
+	}
+
+	// Make menu items and logo bigger, and hide the black filler 
+	// that helps the transition/fade seem smoother than fading the gradient
+	function expandMainMenu() {
+		TweenMax.to($(".main-menu-item"), .5, {height: "4em", lineHeight: "4em", fontSize: "2em"});
+		TweenMax.to($("#main-menu-logo"), .5, {height: "6em"});
+		TweenMax.to($(".header-filler"), .8, {opacity: 0});
+	}
+
+	// Make menu items and logo smaller, and show the black filler 
+	// that helps the transition/fade seem smoother than fading the gradient
+	function shrinkMainMenu() {
+		TweenMax.to($(".main-menu-item"), .5, {height: "3.25em", lineHeight: "3.25em", fontSize: "1.6em"});
+		TweenMax.to($("#main-menu-logo"), .5, {height: "5.5em"});
+		TweenMax.to($(".header-filler"), .8, {opacity: 1});	    
+	}
+
+	// ----------- Setup other page elements -----------
+	function setupOtherPageElements() {
+		// Bouncing Arrow on top slide moves to next slide
+		$(".more-arrow").click(function() {
+			$.fn.fullpage.moveSectionDown();
+		});
+	}
+})(jQuery);
